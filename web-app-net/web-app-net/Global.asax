db980c382e3b6c6efaffa527a58b507a597d8217
Benjamin Villanueva <benjysandra@hotmail.com>
2015-06-15 15:54:48 -0500

    added a title change

---
+++ web-app-net/web-app-net/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="web_app_net.MvcApplication" Language="C#" %>
---
+++ web-app-net/web-app-net/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(web_app_net.Startup))]
+namespace web_app_net
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
